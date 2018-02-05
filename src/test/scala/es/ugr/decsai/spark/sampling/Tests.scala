40e25b9f780e45a1a877ef682ac9b9883dcb2e7e
Daniel Trujillo Viedma <dtviedma@ujaen.es>
2018-02-05 12:07:14 +0100

    Delete potentially disagreeable GLaDOS test joke

--- src/test/scala/es/ugr/decsai/spark/sampling/Tests.scala
+++ src/test/scala/es/ugr/decsai/spark/sampling/Tests.scala
@@ -6,39 +6,6 @@ import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
 
 /*
- * 
- * "Well done. Here are the test results: You are a horrible person.
- * I'm serious, that's what it says: "A horrible person." We weren't
- * even testing for that. Don't let that horrible-person thing discourage
- * you. It's just a data point. If it makes you feel any better, science
- * has now validated your birth mother's decision to abandon you on a doorstep." - GLaDOS
- * 
- * 
-            ,:/+/-
-            /M/              .,-=;//;-
-       .:/= ;MH/,    ,=/+%$XH@MM#@:
-      -$##@+$###@H@MMM#######H:.    -/H#
- .,H@H@ X######@ -H#####@+-     -+H###@X
-  .,@##H;      +XM##M/,     =%@###@X;-
-X%-  :M##########$.    .:%M###@%:
-M##H,   +H@@@$/-.  ,;$M###@%,          -
-M####M=,,---,.-%%H####M$:          ,+@##
-@##################@/.         :%H##@$-
-M###############H,         ;HM##M$=
-#################.    .=$M##M$=
-################H..;XM##M$=          .:+
-M###################@%=           =+@MH%
-@################M/.          =+H#X%=
-=+M##############M,       -/X#X+;.
-  .;XM##########H=    ,/X#H+:,
-     .=+HM######M+/+HM@+=.
-         ,:/%XM####H/.
-              ,.:=-.
- */
-
-
-
-/*
  * This tests depends heavily on a random generator, causing some tests to fail sometimes
  */
 class Tests extends FlatSpec {
@@ -60,7 +27,7 @@ class Tests extends FlatSpec {
   it should "double the minclass instances with percentage = 200" in {
     val oversampled = runROS(dataset, "min", "maj",200)
     
-    val mincount = oversampled.filter { x => x.split(",").last.contentEquals("min") }.count()
+    val mincount = oversampled.filter { x => x.label.contentEquals("min") }.count()
     val majcount = oversampled.filter { x => x.split(",").last.contentEquals("maj") }.count()
     
     assert((mincount-majcount*2)<=2)
